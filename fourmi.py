9b4840cd648ed79760e5874ead09fc3d06531313
0x876 <108541641+0x876-gh@users.noreply.github.com>
2024-02-03 12:41:54 +0100

    Add files via upload

---
+++ fourmi.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import random
+import json
+
+largeur = 200
+hauteur = 200
+
+# position de depart
+y = round(hauteur/2)
+x = round(largeur/2)
+orientations = ["N","E","S","O"]
+Ori = random.choice(orientations)
+
+save_time = 0
+
+
+Matrice = [[0 for i in range(largeur)] for i in range(hauteur)]
+
+while True:
+    if Ori =="N":
+        x-=1
+    if Ori =="S":
+        x+=1
+    if Ori =="E":
+        y+=1
+    if Ori =="O":
+        y-=1
+
+    if Matrice[x][y]==0:
+        Matrice[x][y]=1
+        try:
+            Ori = orientations[orientations.index(Ori)-1]
+            print(Ori,"c'est passé")
+        except:
+            Ori = orientations[3]
+            print(Ori,"c'est pas passé")
+    elif Matrice[x][y]==1:
+        Matrice[x][y]=0
+        try:
+            Ori = orientations[orientations.index(Ori)+1]
+            print(Ori,"c'est passé")
+        except:
+            Ori = orientations[0]
+            print(Ori,"c'est pas passé")
+
+    plt.imshow(Matrice)
+    plt.pause(0.01)
+    plt.clf()
